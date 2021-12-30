48339d9d82baff7cfaa08112de2467742dfa58d5
SquishyFishy69 <shaunszeyh@gmail.com>
2021-12-30 15:39:43 +0800

    Created form validation and updated style.css

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is a website which can allow you to keep track of the books which you have read or not read before. 
+Updated on 29 Dec 2021 to include validation for forms.
--- script.js
+++ script.js
@@ -1,123 +1,143 @@
 const addButton = document.getElementById("add");
 const bodyDiv = document.getElementsByClassName("body")[0];
-let myLibrary = [];
+const myLibrary = JSON.parse(localStorage.getItem("mylibrary")) || [];
 
 class Book {
-    constructor (
-        title = "Unknown",
-        author = "Unknown",
-        pages = 0,
-        read = false
-    ) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
+  constructor(title = "Unknown", author = "Unknown", pages = 0, read = false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
-const addBookToLibrary = () => {
-    let title = prompt("Enter the title of the book");
-    let author = prompt("Enter the author of the book");
-    let pages = prompt("Enter the number of pages");
-    let read = prompt("Have you read the book? y/n");
-    read = (read === 'y') ? true : false;
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].author == author && myLibrary[i].title == title) {
-            alert("You have entered an existing book!");
-            return;
-        } else if (!read > 0 || read % 1 != 0) {
-            alert("You have not entered an integer for the page count!");
-            return;
-        }
-    }
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-    loopBooks()
+const showForm = () => {
+  const form = document.querySelector(".form");
+  switchDisplay();
+  form.onsubmit = addBookToLibrary;
 };
+
+function switchDisplay() {
+  const form = document.querySelector(".form");
+  form.style.display =
+    form.style.display == "flex"
+      ? (form.style.display = "none")
+      : (form.style.display = "flex");
+}
+
+const addBookToLibrary = (e) => {
+  e.preventDefault();
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const pages = document.getElementById("pages");
+  const read = document.getElementById("read");
+
+  if (
+    title.checkValidity() &&
+    author.checkValidity() &&
+    pages.checkValidity() &&
+    read.checkValidity()
+  ) {
+    const titleVal = title.value;
+    const authorVal = author.value;
+    const pagesVal = pages.value;
+    const readVal = read.checked;
+    const newBook = new Book(titleVal, authorVal, pagesVal, readVal);
+
+    addBook(newBook);
+    switchDisplay();
+  }
+};
+
+function addBook(book) {
+  myLibrary.push(book);
+  localStorage.setItem("mylibrary", JSON.stringify(myLibrary));
+  loopBooks();
+}
 
 const loopBooks = () => {
-    while (bodyDiv.firstChild) {
-        bodyDiv.removeChild(bodyDiv.lastChild);
+  while (bodyDiv.firstChild) {
+    bodyDiv.removeChild(bodyDiv.lastChild);
+  }
+  myLibrary.forEach(function (book) {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add(
+      book["title"].replace(/\s/g, "").toLowerCase(),
+      "card"
+    );
+
+    const removeBookContainer = document.createElement("div");
+
+    const removeBook = document.createElement("input");
+    removeBook.setAttribute("type", "image");
+    removeBook.setAttribute(
+      "src",
+      "https://cdn3.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-1/253988/11-512.png"
+    );
+
+    removeBook.classList.add("closeBtn", book["title"].split(" ").join(""));
+    removeBook.addEventListener("click", removeBookBtn);
+    removeBookContainer.appendChild(removeBook);
+
+    bookCard.appendChild(removeBookContainer);
+
+    const bookCardBottom = document.createElement("div");
+
+    const title = document.createElement("h2");
+    title.textContent = book["title"];
+    bookCardBottom.appendChild(title);
+
+    const author = document.createElement("p");
+    author.textContent = `Author: ${book["author"]}`;
+    bookCardBottom.appendChild(author);
+
+    const pages = document.createElement("p");
+    pages.textContent = `${book["pages"]} pages`;
+    bookCardBottom.appendChild(pages);
+
+    const read = document.createElement("input");
+    read.setAttribute("type", "button");
+    read.classList.add(book["title"].split(" ").join(""));
+    if (book.read) {
+      read.setAttribute("value", "Read");
+      read.classList.add("readBtn");
+    } else {
+      read.setAttribute("value", "Not read");
+      read.classList.add("notReadBtn");
     }
-    myLibrary.forEach(function (book) {
-        let bookCard = document.createElement('div');
-        bookCard.classList.add(book['title'].split(' ').join(""), 'card');
+    read.addEventListener("click", changeReadClass);
+    bookCardBottom.appendChild(read);
 
-        let removeBookContainer = document.createElement('div');
-        removeBookContainer.setAttribute("style", "display: flex; flex-direction: row; flex-wrap: wrap; justify-content: right;")
-
-        let removeBook = document.createElement('input');
-        removeBook.setAttribute("type", "image");
-        removeBook.setAttribute("src", "https://cdn3.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-1/253988/11-512.png")
-        removeBook.setAttribute("style", "margin: 0; padding: 0; height: 20px; width: 20px;")
-        removeBook.classList.add("closeBtn", book['title'].split(' ').join(""));
-        removeBook.addEventListener("click", removeBookBtn);
-        removeBookContainer.appendChild(removeBook);
-
-        bookCard.appendChild(removeBookContainer);
-
-        let bookCardBottom = document.createElement('div');
-        bookCardBottom.setAttribute("style", "display: flex; flex-direction: column; flex-wrap: wrap; justify-content: center; align-items: center;")
-
-
-        let title = document.createElement('h2');
-        title.textContent = book['title'];
-        title.setAttribute("style", "margin: 5px; padding: 0; font-size: 24px")
-        bookCardBottom.appendChild(title);
-
-        let author = document.createElement('p');
-        author.textContent = `Author: ${book['author']}`;
-        author.setAttribute("style", "margin: 5px; padding: 0;")
-        bookCardBottom.appendChild(author);
-
-        let pages = document.createElement('p');
-        pages.textContent = `${book['pages']} pages`;
-        pages.setAttribute("style", "margin: 5px; padding: 0;")
-        bookCardBottom.appendChild(pages);
-
-        let read = document.createElement('input');
-        read.setAttribute("type", "button")
-        read.classList.add(book['title'].split(' ').join(""));
-        if (book.read) {
-            read.setAttribute("value", "Read");
-            read.classList.add("readBtn")
-        } else {
-            read.setAttribute("value", "Not read");
-            read.classList.add("notReadBtn");
-        }
-        read.addEventListener("click", changeReadClass);
-        bookCardBottom.appendChild(read);
-
-        bookCard.appendChild(bookCardBottom);
-        bodyDiv.appendChild(bookCard);
-    })
+    bookCard.appendChild(bookCardBottom);
+    bodyDiv.appendChild(bookCard);
+  });
 };
 
-addButton.addEventListener("click", addBookToLibrary, false);
+addButton.addEventListener("click", showForm, false);
 window.addEventListener("load", loopBooks);
 
 const changeReadClass = (e) => {
-    console.log(e.target);
-    if (e.target.classList[1] === 'readBtn') {
-        e.target.setAttribute("value", "Not read");
-        e.target.classList.replace("readBtn", "notReadBtn");
-    } else {
-        e.target.setAttribute("value", "Read");
-        e.target.classList.replace("notReadBtn", "readBtn");
-    }
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].title == e.target.classList[0]) myLibrary[i].read = !myLibrary[i].read;
-    }
+  console.log(e.target);
+  if (e.target.classList[1] === "readBtn") {
+    e.target.setAttribute("value", "Not read");
+    e.target.classList.replace("readBtn", "notReadBtn");
+  } else {
+    e.target.setAttribute("value", "Read");
+    e.target.classList.replace("notReadBtn", "readBtn");
+  }
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i].title == e.target.classList[0])
+      myLibrary[i].read = !myLibrary[i].read;
+  }
 };
 
 const removeBookBtn = (e) => {
-    let bookID = e.target.classList[1];
-    bodyDiv.removeChild(document.querySelector(`.${e.target.classList[1]}`));
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i]["title"].split(' ').join('') == bookID) {
-            myLibrary.splice(i, 1);
-            break;
-        }
+  const bookID = e.target.classList[1];
+  bodyDiv.removeChild(document.querySelector(`.${e.target.classList[1]}`));
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i]["title"].replace(/\s/g, "").toLowerCase() == bookID) {
+      myLibrary.splice(i, 1);
+      break;
     }
+  }
 };
